d8e7a85a348e0c915e2648c4ade9216930ec3d3b
Davi-Noah <daviollee@gmail.com>
2024-08-03 17:09:54 -0300

    app.py index

--- app.py
+++ app.py
@@ -1,2 +1,11 @@
 from flask import Flask, rende_template, request, redirect
 from database import Contato, session
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    contatos = session.query(Contato).all()
+    return rende_template('index.html', contatos=contatos)
