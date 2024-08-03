089e795dda4a72c0fa6606c5295c4c500cb22111
Davi-Noah <daviollee@gmail.com>
2024-08-03 20:00:09 -0300

    final

--- app.py
+++ app.py
@@ -23,5 +23,29 @@ def salvar_contato():
     session.commit()
     return redirect('/')
 
+
+@app.route('/deletar_contato', methods=['POST'])
+def deletar_contato():
+    email = request.form['email']
+    contato = session.query(Contato).filter_by(email=email).first()
+    if contato:
+        session.delete(contato)
+        session.commit()
+    return redirect('/')
+
+
+@app.route('/editar_contato', methods=['POST'])
+def editar_contato():
+    contato_id = request.form['id']
+    contato = session.query(Contato).get(contato_id)
+    if contato:
+        contato.nome = request.form['nome']
+        contato.email = request.form['email']
+        contato.celular = request.form['celular']
+        contato.celular_alt = request.form.get('celular_alt', '')
+        contato.tags = request.form['tags']
+        session.commit()
+    return redirect('/')
+
 if __name__ == '__main__':
     app.run(debug=True)
