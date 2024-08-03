b5e5d1f3501b0cdf23b4b91009abcc62926f60f5
Davi-Noah <daviollee@gmail.com>
2024-08-03 17:06:20 -0300

    criacao database

---
+++ app.py
@@ -0,0 +1,2 @@
+from flask import Flask, rende_template, request, redirect
+from database import Contato, session
--- database.py
+++ database.py
@@ -5,3 +5,19 @@ engine = create_engine('sqlite:///contatos.db', echo=True)
 
 Base = declarative_base()
 
+class Contato(Base):
+    __tablename__ = 'contatos'
+
+    id = Column(Integer, primary_key=True)
+    nome = Column(String(100), nullable=False)
+    email = Column(String(100), unique=True, nullable=False)
+    celular = Column(String(20), nullable=False)
+    celular_alt = Column(String(20), nullable=True)
+    tags = Column(String(100), nullable=True)
+
+
+Base.metadata.create_all(engine)
+
+Session = sessionmaker(bind=engine)
+session = Session()
+
