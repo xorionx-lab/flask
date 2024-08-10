4a3a50282b2df8b12d2e164aad146359c97f7dff
Pedromiguelbarros2017@gmail.com <gabrielcardososilva290@gmail.com>
2024-08-10 19:25:44 -0300

    final

--- app.py
+++ app.py
@@ -27,22 +27,44 @@ def register():
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    if request.method == "POST":
+        username = request.form['username']
+        password = request.form['password']
+        user = session.query(User).filter_by(username=username).first()
+
+        if user and check_password_hash(user.password, password):
+            flask_session['user_id'] = user.id
+            return redirect('/')
+        else:
+            error = "Login falhou! Verifique suas credenciais ou "
+            register_link = "<a href='/register'>registre-se aqui</a>."
+            return render_template('login.html', error=error + register_link)
+
     return render_template('login.html')
 
 
 @app.route('/')
 def index():
-    contatos = session.query(Contato).all()
+    if 'user_id' not in flask_session:
+        return redirect('/login')
+
+    user_id = flask_session['user_id']
+    contatos = session.query(Contato).filter_by(user_id=user_id).all()
     return render_template('index.html', contatos=contatos)
 
 @app.route('/salvar_contato', methods=['POST'])
 def salvar_contato():
+    if 'user_id' not in flask_session:
+        return redirect('/login')
+    
+    user_id = flask_session['user_id']
     novo_contato = Contato(
         nome = request.form['nome'],
         email = request.form['email'],
         celular = request.form['celular'],
         celular_alt = request.form.get('celular_alt', ''),
         tags = request.form['tags'],
+        user_id=user_id
     )
     session.add(novo_contato)
     session.commit()
